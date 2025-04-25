08343385efebcd235f601a0071b9a1e4bbe8914d
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-04-25 15:03:15 +0100

    Update project configurations in app.module.ts and main.ts files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,80 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
+import Redis from 'ioredis';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { RedisStore } from 'connect-redis';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: false,
+        migrations: [],
+        subscribers: [],
+      }),
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        ttl: configService.get<number>('REDIS_TTL') * 1000,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(private readonly configService: ConfigService) {}
+  configure(consumer: MiddlewareConsumer) {
+    const redisClient = new Redis({
+      host: this.configService.get<string>('REDIS_HOST'),
+      port: this.configService.get<number>('REDIS_PORT'),
+    });
+    const RedisStoreInstance = new RedisStore({
+      client: redisClient,
+      ttl: this.configService.get<number>('REDIS_TTL') * 1000,
+    });
+    consumer
+      .apply(
+        session({
+          store: RedisStoreInstance,
+          secret: this.configService.get<string>('SESSION_SECRET'),
+          resave: false,
+          saveUninitialized: false,
+          cookie: {
+            maxAge: this.configService.get<number>('REDIS_TTL') * 1000,
+            httpOnly: true,
+            secure: this.configService.get<string>('NODE_ENV') === 'production', // Set to true in production
+            sameSite: 'lax', // CSRF protection
+            // domain: this.configService.get<string>('COOKIE_DOMAIN'),
+          },
+        }),
+        passport.initialize(),
+        passport.session(),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@bull-board/express';
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { Queue } from 'bullmq';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const expressApp = app.getHttpAdapter().getInstance();
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/admin/queues');
+  createBullBoard({
+    queues: [
+      new BullMQAdapter(
+        new Queue('email', {
+          connection: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+          },
+        }),
+      ),
+    ],
+    serverAdapter,
+  });
+  expressApp.use('/admin/queues', serverAdapter.getRouter());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
